97748c8188876642cd9a0adc95ee32fa20821989
Seth Wax <sethlwax@gmail.com>
2018-05-03 19:58:05 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,48 @@
+require 'pry'
+class Song
+  attr_accessor :name
+  attr_reader :artist, :genre
+  @@all = []
+
+  def initialize(name, artist = nil, genre = nil)
+    self.name = name
+    self.artist = artist if artist
+    self.genre = genre if genre
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    Song.all << self
+  end
+
+  def self.destroy_all
+    Song.all.clear
+  end
+
+  def self.create(name, artist = nil, genre = nil)
+    song = Song.new(name, artist, genre)
+    song.save
+    song
+  end
+
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end
+
+  def genre=(genre)
+    @genre = genre
+    genre.add_song(self)
+  end
+
+  def self.find_by_name(name)
+    self.all.detect {|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create(name)
+  end
+end
